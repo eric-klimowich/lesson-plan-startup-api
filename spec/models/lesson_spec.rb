2a041ca88b27523a33a8270c1e9a9d5f0e6f14d4
eric-klimowich <eklimowich@hotmail.com>
2022-01-13 12:39:19 -0500

    Refactored rspec tests under validations category

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,17 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it "tests lesson object" do
-    lesson = create(:lesson)
-    # expect(lesson.title).to eq("The Title")
-    expect(lesson).to be_valid
-  end
+  describe '#validations' do
+    let(:lesson) { build(:lesson) }
 
-  it "has an invalid title" do
-    lesson = build(:lesson, title: '')
-    expect(lesson).not_to be_valid
-    expect(lesson.errors[:title]).to include("can't be blank")
+    it 'tests lesson object' do
+      expect(lesson).to be_valid
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(lesson).not_to be_valid
+      expect(lesson.errors[:title]).to include("can't be blank")
+    end
   end
+  # it 'tests lesson object' do
+  #   lesson = create(:lesson)
+  #   # expect(lesson.title).to eq("The Title")
+  #   expect(lesson).to be_valid
+  # end
+  #
+  # it 'has an invalid title' do
+  #   lesson = build(:lesson, title: '')
+  #   expect(lesson).not_to be_valid
+  #   expect(lesson.errors[:title]).to include("can't be blank")
+  # end
   # it "tests a number to be positive" do
   #   expect(1).to be_positive # be_positive => 1.positive? == true
   #   expect(5).to be > 3
