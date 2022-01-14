334ac5c5369821a05c08bd02be804e46d22c633f
eric-klimowich <eklimowich@hotmail.com>
2022-01-14 12:46:28 -0500

    Created test for #show path, created route, and method in controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lessons, only: [:index]
+  resources :lessons, only: %i[index show]
 end
--- spec/routing/lessons_spec.rb
+++ spec/routing/lessons_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe '/lessons routes' do
     # expect(get '/lessons').to route_to(controller: 'lessons', action: 'index')
     expect(get '/lessons').to route_to('lessons#index')
   end
+
+  it 'routes to lessons#show' do
+    expect(get('/lessons/1')).to route_to('lessons#show', id: '1')
+  end
 end
