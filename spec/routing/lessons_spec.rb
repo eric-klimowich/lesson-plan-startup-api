e29d6561b69e314fc15adf866087f17f628f29b5
eric-klimowich <eklimowich@hotmail.com>
2022-01-14 11:44:21 -0500

    Fixed route test syntax, added route to index, and created controller with index

--- spec/routing/lessons_spec.rb
+++ spec/routing/lessons_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe '/lessons routes' do
   it 'routes to lessons#index' do
-    # expect(get '/lessons').to route(controller: 'lessons', action: 'index')
-    expect(get '/lessons').to route('lessons#index')
+    # expect(get '/lessons').to route_to(controller: 'lessons', action: 'index')
+    expect(get '/lessons').to route_to('lessons#index')
   end
 end
