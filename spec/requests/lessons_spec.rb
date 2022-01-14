4d219e752368aaa9ccb659c71913cc165b17b0af
eric-klimowich <eklimowich@hotmail.com>
2022-01-14 13:39:20 -0500

    Created test for JSON response

--- spec/requests/lessons_spec.rb
+++ spec/requests/lessons_spec.rb
@@ -6,6 +6,24 @@ RSpec.describe "Lessons", type: :request do
       get "/lessons/index"
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it 'returns a proper JSON' do
+      lesson = create :lesson
+      get '/lessons'
+      body = JSON.parse(response.body)
+      expect(body).to eq(
+        data: [
+          {
+            id: lesson.id,
+            type: 'lessons',
+            attributes: {
+              title: lesson.title,
+              description: lesson.description,
+              slug: lesson.slug
+            }
+          }
+        ]
+      )
+    end
+  end
 end
